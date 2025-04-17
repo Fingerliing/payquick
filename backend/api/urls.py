1a71d6ab81ba5b214e4525c1b9b8b122ff250862
Fiingerling <xela.inotteb@gmail.com>
2025-04-17 12:12:07 +0200

    Ajout de la dépendance djangorestframework-simplejwt pour l'authentification JWT, mise à jour des routes API pour inclure les vues de token d'obtention et de rafraîchissement, et configuration du framework REST pour utiliser l'authentification JWT.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import (
     RestaurantViewSet,
     ClientProfileViewSet,
@@ -17,4 +21,6 @@ router.register(r'menu-items', MenuItemViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('me/', MeView.as_view(), name='me'),
+     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
